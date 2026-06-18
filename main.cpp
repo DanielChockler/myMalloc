cac8cb6461e6b29ae8b15c4709d7051b4339b3f7
Daniel Chockler <daniel.chockler@gmail.com>
2026-06-18 13:27:35 +0100

    Added best fit allocation

--- main.cpp
+++ main.cpp
@@ -15,14 +15,83 @@ struct memBlock {
 
 class Allocator {
 private:
+  size_t m_capacity;
   std::byte* heapStart;
   std::byte* heapEnd;
   bool lock;
   uint32_t blockNum {0};
   uint16_t pageNum {0};
+  
+  memBlock* findLastBlock() {
+    memBlock* block = reinterpret_cast<memBlock*>(heapStart);
+    while (block->next) block = block->next;
+    return block;
+  }
+
+  memBlock* findSmallestBlock(size_t size) {
+    memBlock* currentBlock = reinterpret_cast<memBlock*>(heapStart);
+    memBlock* smallestBlock {nullptr};
+    memBlock* lastBlock = currentBlock;
+    
+    while (currentBlock) {
+      if (currentBlock->marker != BLOCKMARKER) throw std::bad_alloc();
+
+      if (currentBlock->length + sizeof(memBlock) >= size && currentBlock->inUse == false) {
+        if (smallestBlock == nullptr || currentBlock->length < smallestBlock->length) {
+          smallestBlock = currentBlock;
+        }
+      }
+      lastBlock = currentBlock;
+      currentBlock = currentBlock->next;
+    }
+
+    if (!smallestBlock) {
+      memBlock* block = findLastBlock();
+      while (block->length < size) {
+        sbrk(m_capacity);
+        block->length += m_capacity;
+        ++pageNum;
+      }
+      smallestBlock = block;
+    }
+    
+    smallestBlock->inUse = true;
+
+    return smallestBlock;
+  }
+
+  memBlock* allocateBestFit(size_t size) {
+    memBlock* smallestBlock = findSmallestBlock(size);
+    int leftoverSpace = smallestBlock->length - size - sizeof(memBlock);
+    
+    if (smallestBlock->next && leftoverSpace < 1) return smallestBlock; // Smallest block not at end and too small to split
+
+    while (leftoverSpace < 1) {  // Must have at least 1 byte space to allocate something in a new block
+       sbrk(m_capacity);
+      smallestBlock->length += m_capacity;
+      ++pageNum;
+      leftoverSpace = smallestBlock->length - size - sizeof(memBlock);
+    }
+    
+    std::byte* newBlockLocation = reinterpret_cast<std::byte*>(smallestBlock + sizeof(memBlock) + size);
+    memBlock* newBlock = reinterpret_cast<memBlock*>(newBlockLocation);
+    newBlock->marker = BLOCKMARKER;
+    newBlock->inUse = false;
+    newBlock->length = leftoverSpace;
+    newBlock->next = smallestBlock->next;
+    newBlock->prev = smallestBlock;
+    
+    if (newBlock->next) newBlock->next->prev = newBlock;
+    smallestBlock->next = newBlock;
+
+    smallestBlock->length = size;
+    ++blockNum;
+    
+    return smallestBlock;
+  }
 
 public:
-  explicit Allocator(size_t capacity) {
+  explicit Allocator(size_t capacity) : m_capacity(capacity) {
     if (capacity > sizeof(memBlock)) throw std::bad_alloc();
 
     heapStart = reinterpret_cast<std::byte*>(sbrk(0));
@@ -40,11 +109,21 @@ public:
     firstBlock->prev = nullptr;
   }
   
+  // Obey rule of 5, don't want to copy or move the allocator
+  Allocator(const Allocator&) = delete;
+  Allocator& operator=(const Allocator&) = delete;
+  Allocator(Allocator&&) = delete;
+  Allocator& operator=(Allocator&&) = delete;
+  
   std::byte* allocate(size_t size) {
     while (lock) sleep(1);
     lock = true;
 
+    memBlock* block = allocateBestFit(size);
     
+    lock = false;
+
+    return reinterpret_cast<std::byte*>(block) + sizeof(memBlock);
   }
 
 };
